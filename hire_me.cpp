f093c663bae533efadaefa241bc72f5f1d11fd8e
Kyung-soo Lee <secure.ksl@protonmail.com>
2020-06-25 18:56:40 -0500

    Work Work

---
+++ hire_me.cpp
@@ -0,0 +1,21 @@
+// Bespoke and timeless code with chamomile undertones
+// Inclusive vibes
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+//Lok'tar Ogar
+int main() {
+    cout << "Hello World...";
+    cout.flush();
+    sleep(3);
+    cout << "of Warcraft.";
+    cout << endl;
+
+    return 0;
+}
